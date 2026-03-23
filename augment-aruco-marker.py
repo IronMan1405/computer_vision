f921a2674f0503247f1c4ec81564d267ce6da4df
IronMan1405 <daksheshnankani1405@gmail.com>
2026-03-23 11:44:25 +0530

    implemented augment image on aruco and draw aruco contours

---
+++ augment-aruco-marker.py
@@ -0,0 +1,75 @@
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
+def findAruco(img, size=6, totalMarkers=250, draw=True):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    arucoDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+    arucoParams = aruco.DetectorParameters()
+    detector = aruco.ArucoDetector(arucoDict, arucoParams)
+
+    corners, ids, rejected = detector.detectMarkers(gray)
+
+    if ids is not None:
+        if draw:
+            aruco.drawDetectedMarkers(img, corners, ids)
+    else:
+        cv2.putText(img, "no marker detected", (10, 100), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 255, 0), 2)
+
+    return [corners, ids]
+
+def augmentAruco(corners, ids, img, augImg, drawId=True):
+    topleft = corners[0][0][0], corners[0][0][1]
+    topright = corners[0][1][0], corners[0][1][1]
+    bottomright = corners[0][2][0], corners[0][2][1]
+    bottomleft = corners[0][3][0], corners[0][3][1]
+
+    border = np.array([topleft, topright, bottomright, bottomleft])
+
+    h, w, c = img.shape
+    augH, augW, augC = augImg.shape
+
+    imgpts = np.array([[0, 0], [augW, 0], [augW, augH], [0, augH]])
+
+    matrix, _ = cv2.findHomography(imgpts, border)
+
+    imgOut = cv2.warpPerspective(augImg, matrix, (w, h))
+
+    cv2.fillConvexPoly(img, border.astype(int), (0,0,0))
+
+    imgOut = img + imgOut
+
+    return imgOut
+
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    augImg = cv2.imread('assets/Oreki-1.jpeg')
+    
+    while True:
+        success, img = cap.read()
+
+        if not success:
+            break
+        
+        aruco = findAruco(img, draw=False)
+
+        if len(aruco[0]) != 0:
+            for corners, ids in zip(aruco[0], aruco[1]):
+                img = augmentAruco(corners, ids, img, augImg)
+                # augmentAruco(corners, ids, img, augImg)
+
+        cv2.imshow("cam", img)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
---
+++ drawcontours-aruco.py
@@ -0,0 +1,65 @@
+import cv2
+import cv2.aruco as aruco
+import numpy as np
+
+def findAruco(img, size=6, totalMarkers=250, draw=True):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    arucoDict = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
+    arucoParams = aruco.DetectorParameters()
+    detector = aruco.ArucoDetector(arucoDict, arucoParams)
+
+    corners, ids, rejected = detector.detectMarkers(gray)
+
+    if ids is not None:
+        if draw:
+            aruco.drawDetectedMarkers(img, corners, ids)
+    else:
+        cv2.putText(img, "no marker detected", (10, 100), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 255, 0), 2)
+
+    return [corners, ids]
+
+def drawArucoContours(corners, ids, img):
+    if ids is not None:
+        for i, id in enumerate(ids):
+            markerCorners = corners[i][0]
+
+            topleft = (markerCorners[0][0], markerCorners[0][1])
+            topright = (markerCorners[1][0], markerCorners[1][1])
+            bottomleft = (markerCorners[2][0], markerCorners[2][1])
+            bottomright = (markerCorners[3][0], markerCorners[3][1])
+
+            border = np.array([topleft, topright, bottomleft, bottomright], dtype=np.int32)
+
+            cv2.drawContours(img, [border], -1, (0,255,0), 2)
+    
+    else:
+        return
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    augImg = cv2.imread('assets/Oreki-1.jpeg')
+    
+    while True:
+        success, img = cap.read()
+
+        if not success:
+            break
+        
+        aruco = findAruco(img, draw=False)
+        
+        drawArucoContours(aruco[0], aruco[1], img)
+
+        cv2.imshow("cam", img)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
